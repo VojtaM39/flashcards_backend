b08c9ccc5762b20765ebba442d1c1b9f2422ce2d
VojtaM39 <vojtech.maslan1@gmail.com>
2022-07-05 13:21:10 +0200

    Updated docker-compose

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -48,7 +48,7 @@ class AuthService {
   public createToken(user: User): TokenData {
     const dataStoredInToken: DataStoredInToken = { _id: user._id.toString() };
     const secretKey: string = SECRET_KEY;
-    const expiresIn: number = 60 * 60;
+    const expiresIn: number = 60 * 60 * 24;
 
     return { expiresIn, token: sign(dataStoredInToken, secretKey, { expiresIn }) };
   }
